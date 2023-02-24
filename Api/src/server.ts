dfe3fb0ea00576084a671fffb5b06efe17233d97
David Graham <d.graham8@rgu.ac.uk>
2023-02-24 16:27:48 +0000

    api: install cors

--- Api/src/server.ts
+++ Api/src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import { getTelemetry as getTelemetry } from './modules/getTelemetry'
 
 const app = express()
 
 // Set app variables
 app.set('port', process.env.PORT ?? 5000)
+
+// Set app middlewares
+app.use(cors())
 
 // Set routes
 app.get('/rov', async (_, res) => {
