77aa90200c56f65a39a8efd98a872648714b91bd
David Graham <d.graham8@rgu.ac.uk>
2023-03-08 15:39:00 +0000

    update telemetry positions; update global types

--- Api/src/modules/getTelemetry.ts
+++ Api/src/modules/getTelemetry.ts
@@ -9,6 +9,7 @@ export async function getTelemetry(): Promise<RovCampaign> {
 	const data: RovCampaign = {
 		positions: [],
 		times: [],
+		kpi: [],
 		rolls: [],
 		pitches: [],
 		headings: [],
@@ -27,20 +28,33 @@ export async function getTelemetry(): Promise<RovCampaign> {
 		const split = line.split(',') as string[]
 		const [date, time, easting, northing, waterDepth, roll, pitch, heading] = split
 
-		// Date,Time,Easting,Northing,WaterDepth,Roll,Pitch,Heading
-		// 20-02-27,20:50:47.502,0.15818706419820527,0.41015873015873017,92.93,-6.3,2.0,19.9
-
 		const [year, month, day] = (date as string).split('-')
 		const dateTime = new Date(`20${year}-${month}-${day}T${time}`).getTime()
 
-		data.positions.push(eastingOrigin - parseInt(easting))
+		// data.positions.push(eastingOrigin - parseInt(easting))
+		data.positions.push(parseInt(easting))
 		data.positions.push(-1 * parseInt(waterDepth))
-		data.positions.push(northingOrigin - parseInt(northing))
+		// data.positions.push(northingOrigin - parseInt(northing))
+		data.positions.push(parseInt(northing))
 
 		data.times.push(dateTime)
 		data.rolls.push(parseInt(roll))
 		data.pitches.push(parseInt(pitch))
 		data.headings.push(parseInt(heading))
+	}
+
+	const maxInterval = 10000000
+	let tickDifference = 0
+
+	// Normalise times between 0 and 255 for mapping with heatmap gradient in 3D model.
+	for (let i = 0; i < data.times.length - 1; i++) {
+		tickDifference = data.times[i + 1] - data.times[i]
+
+		if (tickDifference >= maxInterval) {
+			tickDifference = maxInterval
+		}
+
+		data.kpi.push(Math.floor(255 * (tickDifference / maxInterval)))
 	}
 
 	return data
--- Api/src/types/RovCampaign.d.ts
+++ Api/src/types/RovCampaign.d.ts
@@ -1,6 +1,7 @@
 export type RovCampaign = {
 	positions: number[]
 	times: number[]
+	kpi: number[]
 	rolls: number[]
 	pitches: number[]
 	headings: number[]
