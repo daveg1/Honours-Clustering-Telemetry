526ebcc0071779e2415c9121f3c4afb52eaea8e9
David Graham <d.graham8@rgu.ac.uk>
2023-03-08 15:26:01 +0000

    fix heatmap gradient

--- Client/src/modules/utils/createHeatMap.ts
+++ Client/src/modules/utils/createHeatMap.ts
@@ -6,15 +6,18 @@ export function createHeatMap(): Globals['heatMap'] {
 	let r = 0;
 	let g = 1;
 	let b = 0;
+	let s = 0;
 
 	for (let i = 0; i < 128; i++) {
-		gradient.push([r, g, b]);
+		gradient.push([r, g, b, s]);
 		r += 1 / 128;
+		s = r;
 	}
 
 	for (let i = 0; i < 128; i++) {
-		gradient.push([r, g, b]);
+		gradient.push([r, g, b, s]);
 		g -= 1 / 128;
+		s += 1 / 128;
 	}
 
 	return gradient;
