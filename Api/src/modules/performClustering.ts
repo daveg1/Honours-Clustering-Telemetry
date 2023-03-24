8a4ffd26e15b97084f82ae66b635ea280037563a
David Graham <d.graham8@rgu.ac.uk>
2023-03-24 10:34:45 +0000

    Api: use CLI arguments to specify data range to perform clustering on

--- Api/src/modules/performClustering.ts
+++ Api/src/modules/performClustering.ts
@@ -3,5 +3,5 @@ import path from 'node:path'
 
 export function performClustering() {
 	const scriptPath = path.resolve(__dirname, '..', 'scripts/dbscan.py')
-	return spawn('python', [scriptPath])
+	return spawn('python', [scriptPath, '10000', '10619']) // * Hardcoded to first leg of journey for now
 }
