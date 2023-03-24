5cb307e4337eda2ffae0a6c53d5c4cee1a42cd3e
David Graham <d.graham8@rgu.ac.uk>
2023-03-24 10:32:35 +0000

    Api: update script to use CLI arguments for data range; simplify script

--- Api/src/scripts/dbscan.py
+++ Api/src/scripts/dbscan.py
@@ -1,28 +1,39 @@
+import sys
 import pandas as pd
 from sklearn.cluster import DBSCAN
 from pathlib import Path
 
+# Remove old denoised datafile
+Path.unlink(Path('../data/telemetry_denoised.csv').resolve(), missing_ok=True)
+
+start = int(sys.argv[1])
+end = int(sys.argv[2])
+
 # Load telemetry
 telemetry = pd.read_csv(Path('../data/telemetry.csv').resolve())
-X = telemetry[['Easting', 'Northing', 'WaterDepth']].head(10619) # for now only first leg of journey
+# Take specified range of data
+X = telemetry.iloc[start:end]
+X = X[['Easting', 'Northing', 'WaterDepth']]
 
 # Perform clustering
 db = DBSCAN(eps=0.15, min_samples=10).fit(X)
-X_labeled = X.copy(deep=True)
-X_labeled['Label'] = db.labels_
-X_labeled.head(5)
 
-# Denoise dataset
-X_denoised = X_labeled[X_labeled['Label'] != -1]
-print(f'{X_labeled.shape[0] - X_denoised.shape[0]} points removed')
+# Label and denoise data
+X['Label'] = db.labels_
+X = X[X['Label'] != -1]
 
 # Add placeholder columns to put file in correct format
-X_denoised.assign(Date='20-02-27')
-X_denoised.assign(Time='20:50:47.502')
-X_denoised.assign(Roll=-6.3)
-X_denoised.assign(Pitch=2.0)
-X_denoised.assign(Heading=19.9)
+X = X.assign(
+	Date='20-02-27',
+	Time='20:50:47.502',
+	Roll=-6.3,
+	Pitch=2.0,
+	Heading=19.9
+)
+
+# Remove label as no longer needed
+X.drop(columns='Label')
 
 # Output to CSV to be parsed by server
 # TODO: Ideally we output directly to JSON so there's one less step
-X_denoised.to_csv(Path('../data/telemetry_denoised.csv').resolve(), index=False)
+X.to_csv(Path('../data/telemetry_denoised.csv').resolve(), index=False)
